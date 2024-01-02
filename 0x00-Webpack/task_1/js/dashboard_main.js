85e0d4dc77899891f9e7a87f9775c1b7122fb5eb
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-02 14:46:56 +0300

    $1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
-import $ from "jquery";
-import _ from "lodash";
+import $ from 'jquery';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -7,9 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let counter = 0;
+let count = 0;
+
 function updateCounter() {
-  counter++;
-    $('#count').html(`${counter} clicks on the button`);
-}
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
+
 $('button').on('click', _.debounce(updateCounter, 500));
