447018b2d95aa8a92fa1e4ddd80b81386163fbf8
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-02 13:20:18 +0300

    $1

---
+++ task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+$(document).ready(function() {
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<p>Copyright - Holberton School</p>");
+});
