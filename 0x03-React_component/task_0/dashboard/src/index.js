cf901d55984e976bd15c9f8f29ef5212713b22ea
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-09 14:57:34 +0300

    correctd index.js so that it wont create error on the console

--- 0x03-React_component/task_0/dashboard/src/index.js
+++ 0x03-React_component/task_0/dashboard/src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App/App'; // Assuming App is your main component
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
