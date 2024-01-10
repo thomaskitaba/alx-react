1543424cebe7b5221e49a4e249b53872c0374395
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-10 16:32:16 +0300

    added task_4 folder

---
+++ 0x03-React_component/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NotificationItem({ type, html, value }) {
+  let li;
+
+  value
+    ? (li = <li data-notification-type={type}>{value}</li>)
+    : (li = (
+        <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
+      ));
+
+  return li;
+}
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  value: '',
+  html: {},
+  markAsRead: () => {},
+
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  markAsRead: PropTypes.func,
+};
+
+export default NotificationItem;
