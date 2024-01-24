96100fcccd50674e2555467388b6f23f253e0f9e
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-24 12:49:02 +0300

    upadgte

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,6 @@
-import {fromJS} from 'immutable';
+import { fromJS } from 'immutable';
 export default function accessImmutableObject(object, array) {
   const obj = fromJS(object);
   const obj1 = obj.getIn(array, 'undefined');
   return obj1;
 }
-
