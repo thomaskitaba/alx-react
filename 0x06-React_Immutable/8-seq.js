7132b05c6b6aac7768cc9972bddba2d6ddffb2c3
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-25 21:16:23 +0300

    update

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,6 +1,6 @@
-import { seq } from 'immutable';
+import { Seq } from 'immutable';
 function printBestStudents(obj) {
-  const seq = seq(obj);
+  const seq = Seq(obj);
   const filtered = seq.filter((stud) => {
     stud.firstName.charAt(0).toUpperCase() + stud.firstName.slice(1);
     stud.lastName.charAt(0).toUpperCase() + stud.lastName.slice(1);
